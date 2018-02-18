01fc3aca3beb56d9e438a59fc583abe68667844f
Gizzz <bulatkhusainov@yandex.ru>
2018-02-18 21:47:05 +0300

    proxy: add requests transformation
    - requests forwarded to specific service
    - requests signed with api key

--- src/proxy/index.ts
+++ src/proxy/index.ts
@@ -7,29 +7,40 @@ import * as dotenv from 'dotenv';
 import axios from 'axios';
 
 dotenv.config();
+
+const key = process.env.GOOGLE_PLACES_API_KEY;
+const serviceUrl = 'https://maps.googleapis.com';
+
 const app = express();
 
 app.get('/proxy*', (req, res) => {
-  const key = process.env.GOOGLE_PLACES_API_KEY;
-  // tslint:disable-next-line:max-line-length
-  const url = `https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=-33.8670522,151.1957362&radius=5000&type=restaurant&keyword=cruise&key=${key}`;
+  const urlToForward = req.url.substr('/proxy'.length);
+  const forwardedUrlWithKey = serviceUrl + urlToForward + `&key=${key}`;
 
-  axios.get(url)
+  axios.get(forwardedUrlWithKey)
     .then((result) => {
-      res.send(result.data);
+      res
+        .status(result.status)
+        .send(result.statusText);
 
-      console.log(`Request succeeded. Data: \n`);
-      console.log(result.data);
-      console.log(`\n/**************************/\n\n`);
+      console.log(`Request succeeded.`);
+      console.log(`Status: ${result.status} - ${result.statusText}`);
+      console.log(`Url: ${forwardedUrlWithKey}`);
+      // Long version:
+      // console.log(`Data: \n${result.data}\n`);
+      console.log(`/**************************/\n`);
     })
     .catch((error) => {
       res
         .status(error.response.status)
         .send(error.response.statusText);
 
-      console.log(`Request failed. Error: \n`);
-      console.log(error);
-      console.log(`\n/**************************/\n\n`);
+      console.log(`Request failed.`);
+      console.log(`Status: ${error.response.status} - ${error.response.statusText}`);
+      console.log(`Url: ${forwardedUrlWithKey}`);
+      // Long version:
+      // console.log(error);
+      console.log(`/**************************/\n`);
     });
 });
 
@@ -39,4 +50,5 @@ app.set('port', (process.env.PORT || 9000));
 
 app.listen(app.get('port'), () => {
   console.log('Google Places API - proxy is running on port', app.get('port'));
+  console.log(`=================================================\n`);
 });
