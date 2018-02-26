5758df29b5f11f001b4ad7e4e700c9edd99708a4
Gizzz <bulatkhusainov@yandex.ru>
2018-02-26 22:23:53 +0300

    move styles and fonts to /assets folder

--- src/client/index.tsx
+++ src/client/index.tsx
@@ -1,7 +1,7 @@
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
 
-import './styles/index.scss';
+import './assets/styles/index.scss';
 import App from './components/App';
 
 ReactDOM.render(
