a48459d98aa94e6464bedc4e74382fe5cd1a4c61
Gizzz <bulatkhusainov@yandex.ru>
2018-02-28 14:43:11 +0300

    rename app name to Poogle (just in logo)

--- src/client/components/components/Header.tsx
+++ src/client/components/components/Header.tsx
@@ -3,7 +3,7 @@ import * as React from 'react';
 const Header = () => (
   <>
     <h1>
-      <span style={{ color: `#4587F1` }}>F</span>
+      <span style={{ color: `#4587F1` }}>P</span>
       <span style={{ color: `#E83F3B` }}>o</span>
       <span style={{ color: `#FABB2A` }}>o</span>
       <span style={{ color: `#4587F1` }}>g</span>
