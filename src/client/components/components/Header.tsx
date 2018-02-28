658e1662777c0fc398aa8649faf3dcef3c734697
Gizzz <bulatkhusainov@yandex.ru>
2018-02-28 14:25:27 +0300

    add description to Header

--- src/client/components/components/Header.tsx
+++ src/client/components/components/Header.tsx
@@ -1,16 +1,21 @@
 import * as React from 'react';
 
 const Header = () => (
-  <h1>
-    <span style={{ color: `#4587F1` }}>F</span>
-    <span style={{ color: `#E83F3B` }}>o</span>
-    <span style={{ color: `#FABB2A` }}>o</span>
-    <span style={{ color: `#4587F1` }}>g</span>
-    <span style={{ color: `#3AA856` }}>l</span>
-    <span style={{ color: `#E83F3B` }}>e</span>
-    {` `}
-    <span style={{ color: `#bbb` }}>4 Places</span>
-  </h1>
+  <>
+    <h1>
+      <span style={{ color: `#4587F1` }}>F</span>
+      <span style={{ color: `#E83F3B` }}>o</span>
+      <span style={{ color: `#FABB2A` }}>o</span>
+      <span style={{ color: `#4587F1` }}>g</span>
+      <span style={{ color: `#3AA856` }}>l</span>
+      <span style={{ color: `#E83F3B` }}>e</span>
+      {` `}
+      <span style={{ color: `#bbbbbb` }}>4 Places</span>
+    </h1>
+    <h3>
+      <span style={{ color: `#bbbbbb` }}>Search Places in Sydney</span>
+    </h3>
+  </>
 );
 
 export default Header;
