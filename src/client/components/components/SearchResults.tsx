ee607cfc904958428cef168526be53bcffdd265f
Gizzz <bulatkhusainov@yandex.ru>
2018-03-01 15:32:39 +0300

    add initial `load more search results` functionality

--- src/client/components/components/SearchResults.tsx
+++ src/client/components/components/SearchResults.tsx
@@ -5,18 +5,18 @@ interface ISearchResultsProps {
   searchResults: any[];
   isFetching: boolean;
   error: any;
+
+  isMoreResultsAvailable: boolean;
+  onLoadMore: () => void;
 }
 
-const SearchResults = ({ term, searchResults, isFetching, error }: ISearchResultsProps) => {
-  if (error) {
-    const message = error.message ? error.message : `Something went wrong.`;
-
-    return (
-      <div className="search-results">
-        <p>Error: {message}</p>
-      </div>
-    );
-  }
+const SearchResults = ({
+  term, searchResults, isFetching, error,
+  isMoreResultsAvailable, onLoadMore,
+}: ISearchResultsProps) => {
+  const errorMessage = error && error.message
+    ? error.message
+    : `Something went wrong.`;
 
   const searchResultsJsx: JSX.Element[] = searchResults.map((result: any): JSX.Element => {
     return (
@@ -38,13 +38,30 @@ const SearchResults = ({ term, searchResults, isFetching, error }: ISearchResult
   return (
     <div className="search-results">
       {
-        isFetching
+        hasResultsToShow
+          ? <ul className="search-results__items">{searchResultsJsx}</ul>
+          : isFetching || isTermEmpty || error
+            ? null
+            : <p>No places found.</p>
+      }
+      {
+        error
+          ? <p>Error: {errorMessage}</p>
+          : null
+      }
+      {
+        isFetching && !error
           ? <p>Loading data...</p>
-          : hasResultsToShow
-            ? <ul className="search-results__items">{searchResultsJsx}</ul>
-            : isTermEmpty
-              ? null
-              : <p>No places found.</p>
+          : null
+      }
+      {
+        isMoreResultsAvailable && hasResultsToShow && !isFetching
+          ? (
+            <button onClick={onLoadMore}>
+              {error ? `Try again` : `Load more`}
+            </button>
+          )
+          : null
       }
     </div>
   );
--- src/client/components/components/SearchResultsContainer.tsx
+++ src/client/components/components/SearchResultsContainer.tsx
@@ -11,6 +11,7 @@ interface ISearchResultsContainerState {
   searchResults: any[];
   isFetching: boolean;
   error: any;
+  nextPageToken: string | null;
 }
 
 class SearchResultsContainer extends React.Component<ISearchResultsContainerProps, ISearchResultsContainerState> {
@@ -18,6 +19,7 @@ class SearchResultsContainer extends React.Component<ISearchResultsContainerProp
     searchResults: [],
     isFetching: false,
     error: null,
+    nextPageToken: null,
   };
 
   componentDidMount() {
@@ -30,6 +32,27 @@ class SearchResultsContainer extends React.Component<ISearchResultsContainerProp
     }
   }
 
+  handleLoadMore = () => {
+    this.setState({ isFetching: true, error: null });
+
+    const url = `/proxy/maps/api/place/nearbysearch/json?pagetoken=${this.state.nextPageToken}`;
+
+    axios.get(url)
+      .then((result) => {
+        this.setState((prevState) => {
+          return {
+            searchResults: [ ...prevState.searchResults, ...result.data.results ],
+            isFetching: false,
+            nextPageToken: result.data.next_page_token ? result.data.next_page_token : null,
+          };
+        });
+      })
+      .catch((error) => {
+        this.setState({ error, isFetching: false });
+        console.log(error);
+      });
+  }
+
   fetchResultsForTerm(term: string) {
     const url = `/proxy/maps/api/place/nearbysearch/json?location=-33.8670522,151.1957362&radius=50000&keyword=${term}`;
 
@@ -38,13 +61,18 @@ class SearchResultsContainer extends React.Component<ISearchResultsContainerProp
       return;
     }
 
-    this.setState({ isFetching: true, error: null });
+    this.setState({
+      searchResults: [],
+      isFetching: true,
+      error: null,
+    });
 
     axios.get(url)
       .then((result) => {
         this.setState({
           searchResults: result.data.results,
           isFetching: false,
+          nextPageToken: result.data.next_page_token ? result.data.next_page_token : null,
         });
       })
       .catch((error) => {
@@ -60,6 +88,9 @@ class SearchResultsContainer extends React.Component<ISearchResultsContainerProp
         searchResults={this.state.searchResults}
         isFetching={this.state.isFetching}
         error={this.state.error}
+
+        isMoreResultsAvailable={this.state.nextPageToken !== null}
+        onLoadMore={this.handleLoadMore}
       />
     );
   }
