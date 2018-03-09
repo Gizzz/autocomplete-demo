e91c20b00e72bae7101b793cdecf64e2c5956f42
Gizzz <bulatkhusainov@yandex.ru>
2018-03-09 21:41:19 +0300

    prepare app for publishing to heroku
    - add heroku build script to npm
    - temporaty disable urlHelper (if heroku works - remove the helper

--- src/client/utils/urlHelper.ts
+++ src/client/utils/urlHelper.ts
@@ -1,12 +1,15 @@
 const getBaseUrl = () => {
   let baseUrl;
 
-  // if client runs in production (github-pages hosting)
-  if (window.location.href.includes(`github.io`)) {
-    baseUrl = `https://autocomplete-demo-awniuaujwv.now.sh`;
-  } else {
-    baseUrl = `http://localhost:9000`;
-  }
+  // try heroku and if works - remove this file
+  baseUrl = ``;
+
+  // // if client runs in production (github-pages hosting)
+  // if (window.location.href.includes(`autocomplete-demo`)) {
+  //   baseUrl = ``;
+  // } else {
+  //   baseUrl = `http://localhost:9000`;
+  // }
 
   return baseUrl;
 };
