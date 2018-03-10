6c79f9701721b4c8f0e8970833c35caea3bdd6e3
Gizzz <bulatkhusainov@yandex.ru>
2018-03-10 14:46:31 +0300

    webpack: add config for dev and prod builds

--- webpack.config.js
+++ webpack.config.js
@@ -1,47 +1,60 @@
+const webpack = require('webpack');
 const CleanWebpackPlugin = require('clean-webpack-plugin');
+const UglifyJsPlugin = require('uglifyjs-webpack-plugin');
 
-module.exports = {
-  entry: './src/client/index.tsx',
-  output: {
-    filename: 'bundle.js',
-    path: __dirname + '/dist/generated',
-    publicPath: 'generated/',
-  },
-  resolve: {
-    extensions: ['.ts', '.tsx', '.js', '.json']
-  },
-  module: {
-    rules: [
-      {
-        test: /\.tsx?$/,
-        loader: 'ts-loader',
-      },
-      {
-        test: /\.scss$/,
-        use: [
-          'style-loader',
-          {
-            loader: 'css-loader',
-            options: { url: false }
-          },
-          'sass-loader'
-        ],
-      },
-    ]
-  },
-  plugins: [
-    new CleanWebpackPlugin(['dist/generated']),
-  ],
-  devtool: 'source-map',
-  devServer: {
-    contentBase: __dirname + '/dist',
-    port: 3000,
-    https: false,
-    compress: true,
-    historyApiFallback: true,
-    hot: false,
-    proxy: {
-      '/proxy': 'http://localhost:9000',
+module.exports = env => {
+  const config = {
+    entry: './src/client/index.tsx',
+    output: {
+      filename: 'bundle.js',
+      path: __dirname + '/dist/generated',
+      publicPath: 'generated/',
     },
-  },
+    resolve: {
+      extensions: ['.ts', '.tsx', '.js', '.json']
+    },
+    module: {
+      rules: [
+        {
+          test: /\.tsx?$/,
+          loader: 'ts-loader',
+        },
+        {
+          test: /\.scss$/,
+          use: [
+            'style-loader',
+            {
+              loader: 'css-loader',
+              options: { url: false }
+            },
+            'sass-loader'
+          ],
+        },
+      ]
+    },
+    plugins: [
+      new CleanWebpackPlugin(['dist/generated']),
+      new webpack.DefinePlugin({
+        'process.env.NODE_ENV': JSON.stringify(env.stage),
+      }),
+    ],
+    devtool: 'source-map',
+    devServer: {
+      contentBase: __dirname + '/dist',
+      port: 3000,
+      https: false,
+      compress: true,
+      historyApiFallback: true,
+      hot: false,
+      proxy: {
+        '/proxy': 'http://localhost:9000',
+      },
+    },
+  };
+
+  if (env.stage === 'production') {
+    config.plugins.push(new UglifyJsPlugin({ sourceMap: true }));
+  }
+
+  return config;
 };
