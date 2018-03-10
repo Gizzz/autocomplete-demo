77a6e38d21cd234bacf623b44b22be3c994d2fa4
Gizzz <bulatkhusainov@yandex.ru>
2018-03-10 16:28:29 +0300

    move styles from assets/ to client/ folder

--- src/client/index.tsx
+++ src/client/index.tsx
@@ -1,7 +1,7 @@
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
 
-import './assets/styles/index.scss';
+import './styles/index.scss';
 import App from './components/App';
 
 ReactDOM.render(
